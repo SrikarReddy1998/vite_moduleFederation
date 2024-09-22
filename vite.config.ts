d114a94960aacc3a97a39a55978bee123d62f5b9
srikarreddy1998 <master.srikarreddy@gmail.com>
2024-09-22 23:54:58 +0530

    installed vite-federation, imported component from other repo and syntax in cite.config.ts, used the exposed component in app.tsx

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    federation({
+      name: "container-app",
+      remotes: {
+        anyModuleName: "http://localhost:4173/assets/remoteEntry.js", // Ensure this URL is correct
+      },
+      shared: ["react", "react-dom"],
+    }),
+  ],
+});
